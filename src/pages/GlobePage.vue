32417c79e9c5873f7c44b125bb208259ff780542
Alik Vovkotrub <alikvovk@icloud.com>
2024-01-25 15:05:18 +0200

    aider: Created a page featuring a Globe using the three-globe library.
    # Aider chat conversation:
    
    USER: Create a page featuring a Globe using three-globe library
    
    ASSISTANT: To create a new page featuring a Globe using the three-globe library, we need to:
    
    1. Create a new Vue component `GlobeComponent.vue` that will use the three-globe library to render a globe.
    2. Create a new Vue page `GlobePage.vue` that will use the `GlobeComponent`.
    3. Add a new route to `routes.ts` for the `GlobePage`.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    src/components/GlobeComponent.vue
    ```vue
    <<<<<<< SEARCH
    =======
    <template>
      <div id="globeContainer"></div>
    </template>
    
    <script lang="ts">
    import { onMounted, ref } from 'vue';
    import * as THREE from 'three';
    import { Globe } from 'three-globe';
    
    export default {
      name: 'GlobeComponent',
      setup() {
        const globeContainer = ref(null);
    
        onMounted(() => {
          const globe = new Globe()
            .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
            .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
            .labelsData([{ lat: 0, lng: 0, name: '0,0' }]);
    
          const renderer = new THREE.WebGLRenderer();
          const scene = new THREE.Scene();
          scene.add(globe);
          const camera = new THREE.PerspectiveCamera();
          camera.position.z = 300;
    
          const animate = () => {
            requestAnimationFrame(animate);
            renderer.render(scene, camera);
          };
          animate();
    
          globeContainer.value.appendChild(renderer.domElement);
        });
    
        return { globeContainer };
      },
    };
    </script>
    
    <style scoped>
    #globeContainer {
      width: 100%;
      height: 100vh;
    }
    </style>
    >>>>>>> REPLACE
    ```
    
    src/pages/GlobePage.vue
    ```vue
    <<<<<<< SEARCH
    =======
    <template>
      <q-page class="row items-center justify-evenly">
        <globe-component></globe-component>
      </q-page>
    </template>
    
    <script lang="ts">
    import GlobeComponent from 'components/GlobeComponent.vue';
    import { defineComponent } from 'vue';
    
    export default defineComponent({
      name: 'GlobePage',
      components: { GlobeComponent },
    });
    </script>
    >>>>>>> REPLACE
    ```
    
    src/router/routes.ts
    ```typescript
    <<<<<<< SEARCH
    const routes: RouteRecordRaw[] = [
      {
        path: '/',
        component: () => import('layouts/MainLayout.vue'),
        children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
      },
    =======
    const routes: RouteRecordRaw[] = [
      {
        path: '/',
        component: () => import('layouts/MainLayout.vue'),
        children: [
          { path: '', component: () => import('pages/IndexPage.vue') },
          { path: 'globe', component: () => import('pages/GlobePage.vue') },
        ],
      },
    >>>>>>> REPLACE
      // Always leave this as last one,
      // but you can also remove it
      {
        path: '/:catchAll(.*)*',
        component: () => import('pages/ErrorNotFound.vue'),
      },
    ];
    
    export default routes;
    ```
    
    Please note that you need to install the `three` and `three-globe` libraries if they are not already installed. You can do this by running `npm install three three-globe` in your project directory.

---
+++ src/pages/GlobePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <globe-component></globe-component>
+  </q-page>
+</template>
+
+<script lang="ts">
+import GlobeComponent from 'components/GlobeComponent.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'GlobePage',
+  components: { GlobeComponent },
+});
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,10 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'globe', component: () => import('pages/GlobePage.vue') },
+    ],
   },
 
   // Always leave this as last one,
